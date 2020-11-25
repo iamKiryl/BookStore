19f3a8b15d3b9efbbdc8f555e7fff03b26654ae6
Kiryl Matusevich <iamkirylm@gmail.com>
2020-11-25 16:22:09 +0100

    Updates

---
+++ precache-manifest.d6fe6136b43b825f9c09951669ca6aa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ecf8c41b39594023e6e65bcf2212ec",
+    "url": "/BookStore/index.html"
+  },
+  {
+    "revision": "e3aec77f058c49c25616",
+    "url": "/BookStore/static/css/main.847ec360.chunk.css"
+  },
+  {
+    "revision": "aa997407a481dd081a09",
+    "url": "/BookStore/static/js/2.76ab25cc.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/BookStore/static/js/2.76ab25cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3aec77f058c49c25616",
+    "url": "/BookStore/static/js/main.93cab5ee.chunk.js"
+  },
+  {
+    "revision": "28c7ea22680ac69b6c7c",
+    "url": "/BookStore/static/js/runtime-main.09f9be6e.js"
+  }
+]);
